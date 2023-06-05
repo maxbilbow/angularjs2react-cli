dda271c815ca30ab807363b2bda995ae9874d05a
maxbilbow <maxbilbow@gmail.com>
2023-06-05 19:55:19 +0100

    Fixed immutable list issue

--- libraries/java/src/main/java/io/github/ng2react/AngularJSToReact.java
+++ libraries/java/src/main/java/io/github/ng2react/AngularJSToReact.java
@@ -5,10 +5,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class AngularJSToReact {
     private static final Logger LOGGER = LoggerFactory.getLogger(AngularJSToReact.class);
@@ -40,13 +37,13 @@ public class AngularJSToReact {
      */
     public Ng2rGenerationResponse convert(Ng2rConvertOptions options) throws IOException {
         LOGGER.debug("Converting component " + options.getComponentName() + " in " + options.getFile() + "...");
-        final List<String> cmd = Arrays.asList(
+        final List<String> cmd = new ArrayList<>(List.of(
                 Ng2rCommand.CONVERT.value(),
                 Objects.requireNonNull(options.getFile()),
                 Objects.requireNonNull(options.getComponentName()),
                 Ng2rOption.SOURCE_ROOT.value(),
                 Objects.requireNonNull(options.getSourceRoot())
-        );
+        ));
         cmd.addAll(getTestOptions(options));
         options.getCustomPrompt().ifPresent(value -> {
             cmd.add(Ng2rOption.CUSTOM_PROMPT.value());
@@ -60,21 +57,21 @@ public class AngularJSToReact {
      */
     public Ng2rGenerationResponse generateReactTest(Ng2rTestGenerationOptions options) throws IOException {
         LOGGER.debug("Generating test for " + options.getFile() + "...");
-        final List<String> cmd = Arrays.asList(
+        final List<String> cmd = new ArrayList<>(List.of(
                 Ng2rCommand.GENERATE_REACT_TEST.value(),
                 Objects.requireNonNull(options.getFile())
-        );
+        ));
         cmd.addAll(getTestOptions(options));
         return cli.call(cmd, Ng2rGenerationResponse.class);
     }
 
     private List<String> getTestOptions(Ng2rTestGenerationOptions options) {
-        final List<String> cmd = Arrays.asList(
+        final List<String> cmd = new ArrayList<>(List.of(
                 Ng2rOption.CWD.value(),
                 Objects.requireNonNull(options.getCwd()),
                 Ng2rOption.API_KEY.value(),
                 Objects.requireNonNull(options.getApiKey())
-        );
+        ));
         options.getOrganization().ifPresent(value -> {
             cmd.add(Ng2rOption.ORGANIZATION.value());
             cmd.add(value);
