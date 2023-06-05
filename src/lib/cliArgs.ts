79b308eac2cfc5528593a5bfac5d454a086ba81b
maxbilbow <maxbilbow@gmail.com>
2023-06-05 12:14:39 +0100

    bundled code into a single file and wrapped in Java API

--- src/lib/cliArgs.ts
+++ src/lib/cliArgs.ts
@@ -1,15 +1,17 @@
-import type { ConvertOptions } from '@ng2react/core'
+import type {ConvertOptions} from '@ng2react/core'
 import {ReactTestOptions} from '@ng2react/core/src/lib/modules/openai-conversion/react-test-gen'
+import {Ng2RCommonOptions, Ng2RConvertOptions, Ng2RTestGenerationOptions} from './generated/CliArgs'
+import {Ng2ROption} from './generated/Options'
 
-export interface CliArgs {
-    cwd: string
-    file: string
+export interface CliArgs extends Ng2RCommonOptions {
     verbose?: true
     quiet?: true
 }
 
 export type FindComponentArgs = CliArgs
 
-export type ConvertComponentArgs = CliArgs & ConvertOptions
+export type ConvertComponentArgs = CliArgs & ConvertOptions & Ng2RConvertOptions
 
-export type GenerateTestArgs = CliArgs & ReactTestOptions
+export type GenerateTestArgs = CliArgs & ReactTestOptions & Ng2RTestGenerationOptions
+
+export type OptionName = Ng2ROption extends `--${infer Name}` ? Name : never
